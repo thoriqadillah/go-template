8be90c137cd1cdf8bdc6a189cb0a73e046ae94f5
thoriqadillah <thoriqadillah59@gmail.com>
2025-02-18 18:56:41 +0700

    refactor test

--- db/db.go
+++ db/db.go
@@ -1,33 +1,31 @@
 package db
 
 import (
-	"app/lib/log"
 	"context"
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	"github.com/stephenafamo/bob"
 )
 
 var sqldb *sql.DB
 var db *bob.DB
-var logger = log.Logger()
 
 func Db() *bob.DB {
 	return db
 }
 
-func Sql() *sql.DB {
-	return sqldb
-}
-
-func Connect(connstr string) (closer func(), err error) {
+func Connect(connstr string) (close func(), err error) {
 	ctx := context.Background()
 
 	pool, err := pgxpool.New(ctx, connstr)
 	if err != nil {
-		logger.Fatal(err.Error())
+		log.Fatal(err.Error())
 		return nil, err
 	}
 
@@ -35,10 +33,63 @@ func Connect(connstr string) (closer func(), err error) {
 	bob := bob.NewDB(sqldb)
 
 	db = &bob
-	close := func() {
+	close = func() {
 		pool.Close()
 		db.Close()
 	}
 
 	return close, sqldb.Ping()
+}
+
+func SetupTest() (purge func()) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %v", err)
+		return
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %v", err)
+	}
+
+	pg, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "17-alpine",
+		Env: []string{
+			"POSTGRES_HOST_AUTH_METHOD=trust",
+			"POSTGRES_DB=apptest",
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+
+	if err != nil {
+		log.Fatalf("Could not start resource: %v", err)
+	}
+
+	pg.Expire(30)
+	hostPort := pg.GetHostPort("5432/tcp")
+	pgUrl := fmt.Sprintf("postgresql://postgres@%s/apptest?sslmode=disable", hostPort)
+
+	// Wait for the Postgres to be ready
+	err = pool.Retry(func() error {
+		_, err := Connect(pgUrl)
+		return err
+	})
+
+	if err != nil {
+		log.Fatalf("Could not connect to postgres: %v", err)
+	}
+
+	return func() {
+		if err := pool.Purge(pg); err != nil {
+			log.Fatalf("Could not purge resource: %v", err)
+		}
+	}
 }
--- db/db_test.go
+++ db/db_test.go
@@ -1,66 +1,16 @@
 package db
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"testing"
 
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
 	"github.com/pressly/goose/v3"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatalf("Could not construct pool: %s", err)
-		return
-	}
-
-	err = pool.Client.Ping()
-	if err != nil {
-		log.Fatalf("Could not connect to Docker: %s", err)
-	}
-
-	pg, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "postgres",
-		Tag:        "17-alpine",
-		Env: []string{
-			"POSTGRES_HOST_AUTH_METHOD=trust",
-			"POSTGRES_DB=apptest",
-			"listen_addresses = '*'",
-		},
-	}, func(config *docker.HostConfig) {
-		// set AutoRemove to true so that stopped container goes away by itself
-		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{
-			Name: "no",
-		}
-	})
-
-	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
-	}
-
-	pg.Expire(30)
-	hostPort := pg.GetHostPort("5432/tcp")
-	pgUrl := fmt.Sprintf("postgresql://postgres@%s/apptest?sslmode=disable", hostPort)
-
-	// Wait for the Postgres to be ready
-	if err := pool.Retry(func() error {
-		_, err := Connect(pgUrl)
-		return err
-	}); err != nil {
-		panic("Could not connect to postgres: " + err.Error())
-	}
-
-	defer func() {
-		if err := pool.Purge(pg); err != nil {
-			log.Fatalf("Could not purge resource: %s", err)
-		}
-	}()
+	purge := SetupTest()
+	defer purge()
 
 	m.Run()
 }
