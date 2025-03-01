268d62ec4ca53b6d3cbd45b17c5ff5ffad430ddd
thoriqadillah <thoriqadillah59@gmail.com>
2025-03-01 12:01:22 +0700

    google oauth

--- go.mod
+++ go.mod
@@ -28,10 +28,12 @@ require (
 	github.com/uptrace/bun/extra/bundebug v1.2.10
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.33.0
+	golang.org/x/oauth2 v0.27.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
--- lib/auth/oauth/google.go
+++ lib/auth/oauth/google.go
@@ -1,6 +1,32 @@
 package oauth
 
-import "context"
+import (
+	"app/env"
+	"context"
+	"encoding/json"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type userInfo struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Locale        string `json:"locale"`
+}
+
+var googleConfig = &oauth2.Config{
+	ClientID:     env.GOOGLE_OAUTH_CLIENT_ID,
+	ClientSecret: env.GOOGLE_OAUTH_SECRET,
+	RedirectURL:  "",         // TODO
+	Scopes:       []string{}, // TODO
+	Endpoint:     google.Endpoint,
+}
 
 type googleOAuth struct{}
 
@@ -9,7 +35,28 @@ func createGoogleOAuth() OAuth {
 }
 
 func (g *googleOAuth) Validate(ctx context.Context, token string) (*User, error) {
-	panic("TODO: implement google oauth")
+	tok := oauth2.Token{AccessToken: token}
+	tokenSource := googleConfig.TokenSource(ctx, &tok)
+	client := oauth2.NewClient(ctx, tokenSource)
+
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var userInfo userInfo
+	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+		return nil, err
+	}
+
+	user := &User{
+		Email: userInfo.Email,
+		Image: userInfo.Picture,
+		Name:  userInfo.Name,
+	}
+
+	return user, nil
 }
 
 func init() {
